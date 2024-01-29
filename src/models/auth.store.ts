2084e7d21a450e09fd03f65a6b593c0059d9ca52
abdizamedmo <abdizamedmo@gmail.com>
2024-01-29 18:33:55 +0300

    logout added in auth store

--- src/models/auth.store.ts
+++ src/models/auth.store.ts
@@ -4,27 +4,35 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import { LoginResult, Profile } from "../services/api.types";
 import api from "../services/api";
 
-interface IAuthStore {
+export interface IAuthStore {
   profile: Profile;
   token: string;
+  isAuthenticated: boolean;
   login(email: string, password: string): Promise<LoginResult>;
+  logout(): void;
 }
 
 export const useAuthStore = create(
   persist<IAuthStore>(
     (set) => ({
       profile: {} as Profile,
+      isAuthenticated: false,
       token: "",
       async login(email, password) {
         try {
           const resp = await api.login(email, password);
 
           const { refreshToken, accessToken, ...rest } = resp?.data;
-          set({ profile: rest, token: accessToken });
+          set({ profile: rest, token: accessToken, isAuthenticated: true });
           return resp;
         } catch (error) {
+          set({ isAuthenticated: false });
           throw error;
         }
+      },
+
+      logout() {
+        set({ isAuthenticated: false, profile: {} as Profile, token: "" });
       },
     }),
     {
